43f655f07deea6e3ffa6973bbbbcb78c75b4991d
FabrizzioMachado <139220822+FabrizzioMachado@users.noreply.github.com>
2023-08-31 15:29:34 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from kivy.app import App
+from kivy.uix.image import Image
 from kivy.uix.video import Video
 from kivy.uix.button import Button
 from kivy.uix.label import Label
@@ -12,12 +13,12 @@ class Temperation(App):
         layout = FloatLayout()
 
         # Aggiungi il video di sfondo
-        video = Video(source='SOLE.mp4', state='play', options={'eos': 'loop'})
-        video.allow_stretch = True
-        video.size_hint = (2.95, 2.9)
-        video.pos_hint = {"center_x": 0.5, "center_y": 0.5}
-        video.opacity = 0.60
-        layout.add_widget(video)
+        imagi = Image(source="immagine_2023-08-31_092336991.png"))
+        imagi.allow_stretch = True
+        imagi.size_hint = (2.95, 2.9)
+        imagi.pos_hint = {"center_x": 0.5, "center_y": 0.5}
+        imagi.opacity = 0.60
+        layout.add_widget(imagi)
 
         # Aggiungi il titolo
         title_label = Label(text='TEMPERATION', bold=True, underline=True, italic=True,  font_size='45sp', pos_hint={"center_x": 0.5, "center_y": 0.8}, size_hint=(1, 0.1))
